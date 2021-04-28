cf73a2bf911c139ecc881f0c43d29e262239addd
EliorYomTov <39540940+EliorYomTov@users.noreply.github.com>
2021-04-28 17:35:12 +0300

    Delete clientAscBalanceSortable.java

--- clientAscBalanceSortable.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class clientAscBalanceSortable implements Comparator<Client> {
-	@Override
-	public int compare(Client o1, Client o2) {
-		return (int) (o2.getAccount().getBalance() - o1.getAccount().getBalance());
-	}
-
-}
