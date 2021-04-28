dba2b92f5cdd64534d462d3193d7e56e37ed68ab
EliorYomTov <39540940+EliorYomTov@users.noreply.github.com>
2021-04-28 17:35:18 +0300

    Delete clientAscIdSortable.java

--- clientAscIdSortable.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class clientAscIdSortable implements Comparator<Client> {
-	@Override
-	public int compare(Client o1, Client o2) {
-		return o1.getId() - o2.getId();
-	}
-}
